27a63dd2e156a2014db8b4a02d5c436b1b26622d
Edwin <yaramarlon256@gmail.com>
2023-08-30 09:31:22 -0500

    function JS

--- 30-08-2023.js
+++ 30-08-2023.js
@@ -1,45 +1,3 @@
-## curso practico de javaScript
-
-# Introducción
-
-# Prueba de JavaScipt
-
- variables
-    
- funciones
- condicionales
- ciclos
- arrays y objetos
- Bonus: reducción de condicionales
- ¡Es tuturno: crea un tutorial!
-
-# Manipulación del DOM
-
- cómo conectar JavaScript con html
- Leyendo HTML desde JavaScript
- Escribiendo HTML desde JavaScript
- Eventos en JavaScript: interactuando con usuarios
- addEventListener
-
-# contribución a proyectos
-
- conectando Github a proyectos de javascript
- Proyectos con JavaScript y Github desde cero
- Fork a proyectos en Github
- Analizando código de proyectos open-source
- Fusion del menú en desktop
- Fusión del menú en mobile
- carrito de compras
- lista de productos: HTML a partir de arrays
- Detalles de un producto
- Interacción entre todos los componentes
- Pull Requests; aportando código a proyectos en Github
- Feedback: conoce proyectos
-
-# Despliegue
-
- Deploy con Github pages
-
 const obj = {
     nombre: 'fulanito',
     edad: 3,
@@ -113,4 +71,4 @@ function conseguirTipoSuscripcion(suscripcion){
 
     console.warn('ese tipo de suscripcion no existe')
 }
-operador logico OR (||)
+
