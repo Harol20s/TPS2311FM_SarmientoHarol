4c1f486d2c320bd17018c139ed5e950e1292bfac
d4nielacaro <nicoldanielacaro@gmail.com>
2023-09-04 12:13:21 -0500

    23

--- 20230830_Script.js
+++ 20230830_Script.js
@@ -2,7 +2,7 @@ console.log("Hola patico")
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
 const parrafito = document.querySelector('parrafito');
-const pid = document.querySelector('pid');
+const pid = document.getElementsByClassName('pid');
 const input = document.querySelector('input');
 
 console.log(input.value)
@@ -25,4 +25,8 @@ h1.classList.remove('azul');
 
 input.value = 'C.C';
 
+const img = document.createElement('img');  
+img.setAttribute('src', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FIMG_%2528company%2529&psig=AOvVaw3OjxL8O1V38--1_0FR5m7L&ust=1693933210371000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCIDH3Yy3kYEDFQAAAAAdAAAAABAD');
+console.log(img);
 
+pid.append(img);
