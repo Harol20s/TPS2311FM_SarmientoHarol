61a48f65782a72b348895ecf2564f1e03e108b1f
harol20s <hlsarmiento1@misena.edu.co>
2023-09-11 12:04:15 -0500

    actividad 110923

--- 20230830_Script.js
+++ 20230830_Script.js
@@ -1,32 +1,16 @@
-console.log("Hola patico")
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.querySelector('parrafito');
-const pid = document.querySelector('pid');
-const input = document.querySelector('input');
+const form = document.querySelector('#form');
+const formulario = document.querySelector('#tablita');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const presult = document.querySelector('#result');
 
-console.log(input.value)
+form.addEventListener('submit', sumarInputValues);
 
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input,
-})
-
-h1.innerHTML = 'patito <br> brr';
-h1.innerText = 'patito <br> brr';
-//console.log(h1.getAttribute('class'));
-//h1.setAttribute('class', 'rojo');
-
-h1.classList.add('azul');
-h1.classList.remove('azul');
-
-input.value = 'C.C';
-
-const img = document.createElement('img');  
-img.setAttribute('src', 'https://cdn-images-1.medium.com/max/1600/1*rrRcaZKZsjerUveQqTSx7A.png');
-console.log(img);
-p.innerHTML = img;
-// pid.append(img);
+function sumarInputValues(event) {
+ //console.log({event});
+ event.preventDefault();   
+ const sumainputs = input1.value + input2.value;
+ presult.innerText = "resultado: " +sumainputs;
+} 
