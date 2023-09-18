bd6f1aefe8000c63367671e17699b9b31e2ca2f4
harol20s <hlsarmiento1@misena.edu.co>
2023-09-18 10:28:35 -0500

    pase de parametros

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send('Peticion GET');
+const harol = {
+    apodo: 'h',
+    edad: 19,
+};
+
+//const apodo = harol.apodo;
+//const edad = harol.edad;
+
+const {apodo, edad  } = harol;
+
+apodo
+
+app.get('/:name/:id', (req, res) =>{
+    const {name,id} = req.params;
+
+    res.send(` ${name}:${id}`);
 });
 
 app.post('/', (req, res) =>{
