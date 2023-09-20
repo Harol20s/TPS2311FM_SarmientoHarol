79679196af83d3acd494a4877b0ab06268481c4a
harol20s <hlsarmiento1@misena.edu.co>
2023-09-20 11:05:53 -0500

    ejecutando querys a mysql

--- backend/src/app.js
+++ backend/src/app.js
@@ -49,6 +49,27 @@ connection.connect((err)=>{
     console.log('bd conectada')
 });
 
+//const querySQL = 'SHOW TABLES';'';
+
+// connection.query (querySQL, (err, res)=> {
+    //if (err) throw err;
+    //console.log('respuesta sql', res);
+//})
+
+const insertQuery = `INSERT INTO User(first_name, age) VALUES('juan', '30');`;
+
+connection.query(insertQuery, (err, res)=>{
+    if(err) throw err;
+    console.log('respuesta insert', res)
+})
+
+const getQuery = `SELECT * FROM User;`;
+
+connection.query(getQuery, (err, res) =>{
+    if(err) throw err;
+    console.log('respuesta get', res)
+})
+
 app.listen(3000, ()=>{
     console.log('Servidor encendido');
 });
