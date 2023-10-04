d69448b4dca3cec46b4c7cc825ba43c546438531
harol20s <hlsarmiento1@misena.edu.co>
2023-10-04 08:30:10 -0500

    index

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,12 +1,16 @@
-const app = require('./app'); 
-const database = require ('./database');
+const app = require ('./app'); 
+const database = require('./database');
 
 const main = () => {
+
     database.connect((err)=> {
         if (err) throw err;
         console.log('base de datos conectada');
     });
+
     app.listen(3000, ()=>{
         console.log('Servidor escuchando puerto 3000');
     });
 };
+
+main();
